47fae5a48eec0e2ab8d7715bdf1ef1b7ff7fd838
huraira52 <hurairajutt52@gmail.com>
2023-11-01 16:37:56 +0500

    form-

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import SignUp from "./SignUp";
+import Registration from "./Registration";
+
+const App = () => {
+  // return <SignUp />;
+  return <Registration />;
+};
+
+export default App;
+
+
+
